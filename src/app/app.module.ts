d988238b6cd1dff2ba6742629aa0658f4e2df654
J6-Babbage <edwinjpr11@gmail.com>
2022-10-31 08:50:18 -0500

    chore: add some pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppRouterModule } from './app-router.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { VentasModule } from './ventas/ventas.module';
+
+// Cambiar el locale de la app
+import localeEs from '@angular/common/locales/es-CO';
+import localeFr from '@angular/common/locales/fr';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localeEs);
+registerLocaleData(localeFr);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRouterModule, SharedModule, VentasModule],
+  providers: [{ provide: LOCALE_ID, useValue: 'es-CO' }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
