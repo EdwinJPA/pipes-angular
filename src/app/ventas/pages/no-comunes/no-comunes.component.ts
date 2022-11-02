1883901ddfc80c9c1b7f2c292032bddc4075eafd
J6-Babbage <edwinjpr11@gmail.com>
2022-11-02 11:40:39 -0500

    feat: pipes personalizados

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
@@ -39,4 +40,19 @@ export class NoComunesComponent {
     edad: 23,
     direccion: 'San Gil, Santander',
   };
+
+  // JsonPipe
+  heroes = [
+    { nombre: 'Superman', vuela: true },
+    { nombre: 'Batman', vuela: true },
+    { nombre: 'Spiderman', vuela: true },
+  ];
+
+  // Async
+  miObservable = interval(5000); // 0,1,2,3,4,5,6
+  valorPromesa = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Tenemos data de la promesa');
+    }, 3500);
+  });
 }
